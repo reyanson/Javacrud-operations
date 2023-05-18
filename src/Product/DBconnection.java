0ef58e61845c32d0b26a39d102ba5cf2474f4aea
reyanson <regirajeswaran_2019508@fot.ruh.ac.lk>
2023-05-19 01:19:46 +0530

    Java CRUD operation

--- src/Product/DBconnection.java
+++ src/Product/DBconnection.java
@@ -1,13 +1,7 @@
 package Product;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  *
  * @author reyanson
